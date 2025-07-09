d450ded5ef2f670f1cc878de1338fda16ece0323
Mickael Stanislas <contact@mickael-stanislas.com>
2025-07-09 11:13:32 +0200

    feat(regex): add T0 & EdgeGateway name

--- internal/regex/regex.go
+++ internal/regex/regex.go
@@ -8,6 +8,11 @@ import (
 const (
 	uUID4RegexString        = `[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}`
 	uRNWithUUID4RegexString = `(?m)urn:[A-Za-z0-9][A-Za-z0-9-]{0,31}:([A-Za-z0-9()+,\-.:=@;$_!*']|%[0-9A-Fa-f]{2})+` + uUID4RegexString
+
+	// * Cloudavenue
+	// edgegateway(t1) name (tn01e02ocb0001234spt101)
+	t0NameRegexString          = `^pr(vrf)?(?<siteCode>[0-9]{2})?(?<workloadType>[a-z]{1})(?<contractId>[a-z0-9]{10})(?<serviceType>[a-z]{2,6})(?<linkType>[a-z]{2})(?<increment>[0-9]{2,3})`
+	edgeGatewayNameRegexString = `^tn(?<siteCode>[0-9]{2})?(?<workloadType>[a-z]{1})(?<workload>[0-9]{2})(?<contractId>[a-z0-9]{10})(?<serviceType>[a-z]{2,6})t1(?<increment>[0-9]{2,5})`
 )
 
 func lazyRegexCompile(str string) func() *regexp.Regexp {
@@ -22,6 +27,8 @@ func lazyRegexCompile(str string) func() *regexp.Regexp {
 }
 
 var (
-	UUID4Regex        = lazyRegexCompile(uUID4RegexString)
-	URNWithUUID4Regex = lazyRegexCompile(uRNWithUUID4RegexString)
+	UUID4Regex           = lazyRegexCompile(uUID4RegexString)
+	URNWithUUID4Regex    = lazyRegexCompile(uRNWithUUID4RegexString)
+	T0NameRegex          = lazyRegexCompile(t0NameRegexString)
+	EdgeGatewayNameRegex = lazyRegexCompile(edgeGatewayNameRegexString)
 )
--- internal/regex/regex_test.go
+++ internal/regex/regex_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestUUID4Regex(t *testing.T) {
+func TestRegex_UUID4(t *testing.T) {
 	tests := []struct {
 		name          string
 		input         string
@@ -47,7 +47,7 @@ func TestUUID4Regex(t *testing.T) {
 	}
 }
 
-func TestURNRegex(t *testing.T) {
+func TestRegex_URN(t *testing.T) {
 	tests := []struct {
 		name          string
 		input         string
@@ -76,6 +76,80 @@ func TestURNRegex(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			matches := URNWithUUID4Regex().FindAllString(test.input, -1)
+			if len(matches) == 0 && !test.expectedError {
+				t.Errorf("Expected to find a match for %s, but found none", test.input)
+			}
+			if len(matches) > 1 && !test.expectedError {
+				t.Errorf("Expected only one match for %s, but found %d", test.input, len(matches))
+			}
+			if len(matches) == 1 && matches[0] != test.expected {
+				t.Errorf("Expected match %s, got %s", test.expected, matches[0])
+			}
+		})
+	}
+}
+
+func TestRegex_T0Name(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		expected      string
+		expectedError bool
+	}{
+		{
+			name:          "Valid T0 Name",
+			input:         "prvrf01eocb0001234allsp01",
+			expected:      "prvrf01eocb0001234allsp01",
+			expectedError: false,
+		},
+		{
+			name:          "Invalid T0 Name",
+			input:         "invalid-name",
+			expected:      "",
+			expectedError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			matches := T0NameRegex().FindAllString(test.input, -1)
+			if len(matches) == 0 && !test.expectedError {
+				t.Errorf("Expected to find a match for %s, but found none", test.input)
+			}
+			if len(matches) > 1 && !test.expectedError {
+				t.Errorf("Expected only one match for %s, but found %d", test.input, len(matches))
+			}
+			if len(matches) == 1 && matches[0] != test.expected {
+				t.Errorf("Expected match %s, got %s", test.expected, matches[0])
+			}
+		})
+	}
+}
+
+func TestRegex_EdgeGatewayName(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		expected      string
+		expectedError bool
+	}{
+		{
+			name:          "Valid Edge Gateway Name",
+			input:         "tn01e02ocb0001234spt101",
+			expected:      "tn01e02ocb0001234spt101",
+			expectedError: false,
+		},
+		{
+			name:          "Invalid Edge Gateway Name",
+			input:         "invalid-name",
+			expected:      "",
+			expectedError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			matches := EdgeGatewayNameRegex().FindAllString(test.input, -1)
 			if len(matches) == 0 && !test.expectedError {
 				t.Errorf("Expected to find a match for %s, but found none", test.input)
 			}
