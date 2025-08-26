85cc58647e9dc18332ab353667c7411625d0ae5c
Mickael Stanislas <contact@mickael-stanislas.com>
2025-08-26 17:07:28 +0200

    chore: move regex to exported package (#30)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- strcase/goname.go
+++ strcase/goname.go
@@ -1,3 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Orange
+ * SPDX-License-Identifier: Mozilla Public License 2.0
+ *
+ * This software is distributed under the MPL-2.0 license.
+ * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
+ * or see the "LICENSE" file for more details.
+ */
+
 package strcase
 
 import (
@@ -140,7 +149,7 @@ var commonInitialisms = map[string]bool{
 	"XSS":   true,
 }
 
-// customInitialisms is a set of common initialisms we use at Scaleway.
+// customInitialisms is a set of common initialisms we use at Cloudavenue.
 // value[0] is the uppercase replacement
 // value[1] is the lowercase replacement
 var customInitialisms = map[string][2]string{
